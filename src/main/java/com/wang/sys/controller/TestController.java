10ac081ec8b9ab42ebf281122424f187c7082806
wangning <wangningning@beyondsoft.com>
2020-11-12 12:42:16 +0800

    测试

--- src/main/java/com/wang/sys/controller/TestController.java
+++ src/main/java/com/wang/sys/controller/TestController.java
@@ -1,9 +1,9 @@
 package com.wang.sys.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/test")
 public class TestController {
 
